47f87466cb91fc22bd3d4f76a9beb048f9568fcb
Fazliddin7007 <fazliddinhonosimov@gmail.com>
2026-03-24 12:39:57 +0500

    Update Project_1.py
    fixing the second num

--- Project_1.py
+++ Project_1.py
@@ -14,26 +14,22 @@ while True:
         print(f'√{a} = {sqrt(a)}')
         continue
     b = int(input('Input second number: '))
+    if op in ('/', '//', '%'):
+        while b == 0:
+            print('Your number is zero')
+            print('Input again: ', end='')
+            b = int(input())
     if op == '+':
         print(f'{a} + {b} = {a + b}')
     elif op == '-':
         print(f'{a} - {b} = {a - b}')
     elif op == '/':
-        if b != 0:
-            print(f'{a} / {b} = {a / b}')
-        else:
-            print('Second number is zero!')
+        print(f'{a} / {b} = {a / b}')
     elif op == '*':
         print(f'{a} * {b} = {a * b}')
     elif op == '//':
-        if b != 0:
-            print(f'{a} // {b} = {a // b}')
-        else:
-            print('Second number is zero!')
+        print(f'{a} // {b} = {a // b}')
     elif op == '%':
-        if b != 0:
-            print(f'{a} % {b} = {((a % b) + b) % b}')
-        else:
-            print('Second number is zero!')
+        print(f'{a} % {b} = {((a % b) + b) % b}')
     elif op == '**':
         print(f'{a} ** {b} = {a**b}')
